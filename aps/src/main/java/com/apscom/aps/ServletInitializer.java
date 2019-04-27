bf0066375376a84b1cd8ef8d6d46a0bc44dbcde7
kaynrhaast <44171763+kaynrhaast@users.noreply.github.com>
2019-04-27 15:08:50 -0300

    upload dos arquivos

---
+++ aps/src/main/java/com/apscom/aps/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.apscom.aps;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ApsApplication.class);
+	}
+
+}
